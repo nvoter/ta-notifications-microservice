48db1688991d07d7cd94ff3b205bf8e180bfbd57
nvoter <avmanushkina@edu.hse.ru>
2026-01-04 12:33:56 +0300

    init: notifications microservice

---
+++ src/test/java/org/fcs/notifications/microservice/NotificationsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fcs.notifications.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationsMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
